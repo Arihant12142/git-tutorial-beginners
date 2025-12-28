4c067f541aa0f29410aa47452c96bd79a8f55dee
Arihant12142 <arihant.bokadiya24@pccoepune.org>
2025-12-29 03:11:46 +0530

    Design Completed

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@
 #This is Python file for hello
 import pandas as pd
 import numpy as np
+import sklearn
 print("Hello World")
 
 # Change to Feature Branch
+print("learn git on vs code")
+print("End")
